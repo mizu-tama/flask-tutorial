f0adf5f0eac6013535439a23abbf7e5825e1bd1a
mizu-tama <tamaki.mizuno@gmail.com>
2021-07-13 15:04:25 +0900

    bugfix: test_login()のバグ修正

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -32,9 +32,9 @@ def test_register_validate_input(client, username, password, message):
 
 
 def test_login(client, auth):
-    assert client.get('/auth/login').status_cade == 200
+    assert client.get('/auth/login').status_code == 200
     response = auth.login()
-    assert response.headers['Location'] == 'http://localhost'
+    assert response.headers['Location'] == 'http://localhost/'
 
     with client:
         client.get('/')
